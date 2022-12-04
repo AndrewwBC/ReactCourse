7779bd943ed37665f30591b2f3fec807374e97bc
AndrewwBC <andrew-hot2011@hotmail.com>
2022-12-04 04:29:51 -0300

    Resolution

--- src/App.js
+++ src/App.js
@@ -1,59 +1,32 @@
 import React from 'react';
-//import Produto from './Produto';
+import Produto from './Produto';
+
+// Need to fix render of Produto
 
 const App = () => {
-  //
-  //const [state, setState] = React.useState(false);
-  //   const [data, setData] = React.useState(null);
-  //   React.useEffect(() => {
-  //     fetch('https://ranekapi.origamid.dev/json/api/produto/notebook').then(
-  //       (response) => response.json().then((json) => setData(json)),
-  //     );
-  //   }, []);
-  //   return (
-  //     <>
-  //       <button onClick={() => setState(!state)}>
-  //         {state ? 'Abra' : 'Feche'}
-  //       </button>
-  //       <div>
-  //         {data && (
-  //           <div>
-  //             <h1>{data.nome}</h1>
-  //             <p>{data.preco}</p>
-  //           </div>
-  //         )}
-  //       </div>
-  //     </>
-  //   );
-
-  const [data, setData] = React.useState(null);
-  const [notebook, setNotebook] = React.useState(false);
-  const [smartphone, setSmartphone] = React.useState(false);
+  const [active, setActive] = React.useState(false);
+  const getStorage = localStorage.getItem('produto');
 
   React.useEffect(() => {
-    fetch('https://ranekapi.origamid.dev/json/api/produto/notebook').then(
-      (response) => response.json().then((json) => setData(json)),
-    );
-  }, [notebook]);
+    if (getStorage !== null) {
+      setActive(getStorage);
+    }
+  }, []);
 
   React.useEffect(() => {
-    fetch('https://ranekapi.origamid.dev/json/api/produto/smartphone').then(
-      (response) => response.json().then((json) => setData(json)),
-    );
-  }, [smartphone]);
+    if (active !== false) localStorage.setItem('produto', active);
+  }, [active]);
 
-  window.localStorage.setItem('produto', `${data && data.nome}`);
+  function handleClick({ target }) {
+    setActive(target.innerText);
+  }
 
-  console.log();
   return (
     <>
-      <h2>Preferência:{data && data.nome}</h2>
-      <div style={{ display: 'flex', gap: '24px' }}>
-        <button onClick={() => setNotebook(!notebook)}>Notebook</button>
-        <button onClick={() => setSmartphone(!smartphone)}>Smartphone</button>
-      </div>
-      <h1>{data && data.nome}</h1>
-      <p>R$ {data && data.preco}</p>
+      <h1>Preferencias: {active}</h1>
+      <button onClick={handleClick}>Notebook</button>
+      <button onClick={handleClick}>Smartphone</button>
+      <Produto produto={active} />
     </>
   );
 };
--- src/Produto.js
+++ src/Produto.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
-const Produto = ({ data }) => {
+const Produto = ({ produto }) => {
+  const [data, setData] = React.useState(null);
+  React.useEffect(() => {
+    if (produto !== null) {
+      fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+        .then((response) => response.json())
+        .then((json) => setData(json));
+    }
+  }, [produto]);
+
+  if (data === null) return null;
+
   return (
     <>
       <h1>{data && data.nome}</h1>
